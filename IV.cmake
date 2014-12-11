8130868c300be2320c43df026a030443dad55aa3
Carlos Duelo <carlos.duelo@gmail.com>
2014-12-11 12:41:03 +0100

    add HDF5

--- IV.cmake
+++ IV.cmake
@@ -1,6 +1,6 @@
 
 set(IV_PACKAGE_VERSION 0.1)
-set(IV_DEPENDS  REQUIRED vmmlib)
+set(IV_DEPENDS  REQUIRED vmmlib HDF5)
 
 set(IV_ROOT_VAR IV_ROOT)
 set(IV_REPO_URL https://github.com/carlosduelo/iv.git)
