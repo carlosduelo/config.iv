f62ec98d5705660b034004f59ba4d3b849f963dc
Carlos Duelo <carlos.duelo@gmail.com>
2014-12-11 12:05:35 +0100

    config vmmlib

---
+++ IV.cmake
@@ -0,0 +1,8 @@
+
+set(IV_PACKAGE_VERSION 0.1)
+set(IV_DEPENDS  REQUIRED vmmlib)
+
+set(IV_ROOT_VAR IV_ROOT)
+set(IV_REPO_URL https://github.com/carlosduelo/iv.git)
+set(IV_REPO_TAG master)
+set(IV_FORCE_BUILD ${CI_BUILD})
